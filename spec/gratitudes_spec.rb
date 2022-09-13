33c439e444884986f84533e2250c0d7bf0abf2c0
Miles Cragg <milescragg@gmail.com>
2022-09-13 15:10:31 +0100

    adds class testing challenge

---
+++ spec/gratitudes_spec.rb
@@ -0,0 +1,12 @@
+require 'gratitudes'
+
+RSpec.describe Gratitudes do
+  it "returns 'Be grateful for: bread, water'" do
+    gratitudes = Gratitudes.new
+    gratitudes.add("bread")
+    gratitudes.add("water")
+    result = gratitudes.format
+    expect(result).to eq "Be grateful for: bread, water"
+  end
+end
+    
